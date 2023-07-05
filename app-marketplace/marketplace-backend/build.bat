f2dbd478722a9764ed358e075e5e1f285fdef36d
HomeServer <pablo.vieira.andrade@gmail.com>
2023-07-04 21:16:42 -0300

    Added Docker Scripts

---
+++ app-marketplace/marketplace-backend/build.bat
@@ -0,0 +1,2 @@
+docker build -t backend .
+docker run -d -p 4200:80 --name Backend_Project backend  
---
+++ app-marketplace/marketplace-frontend-fix/build.bat
@@ -0,0 +1,2 @@
+docker build -t frontend
+docker run -d -p 4200:4210 --name angular-app Frontend_Project
