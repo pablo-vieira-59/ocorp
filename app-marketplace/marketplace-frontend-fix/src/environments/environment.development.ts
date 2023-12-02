bd49e323cc9d896829baec52bcc59938114990ce
Pablo <pablo.vieira.andrade@gmail.com>
2023-12-02 13:14:50 -0300

    [OCO-0] Ajuste de configs

--- app-marketplace/marketplace-frontend-fix/src/environments/environment.development.ts
+++ app-marketplace/marketplace-frontend-fix/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    //backendUrl: 'https://localhost:7261/',
-    backendUrl: 'http://ocorp.viewdns.net:4200/',
+    backendUrl: 'https://localhost:7261/',
+    //backendUrl: 'http://ocorp.viewdns.net:4200/',
     cdnUrl: 'http://ocorp.viewdns.net:4201/',
     ambient: "Dev"
 };
